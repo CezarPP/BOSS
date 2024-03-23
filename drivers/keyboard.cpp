4a305b44a5eb7139f33d2101a9e3b6c0cf8ce3a5
cezar <cezarpopoveniuc51@gmail.com>
2024-03-23 18:18:05 +0200

    Refactor keyboard driver to use keyMap and handle uppercase keys with shift.

--- drivers/keyboard.cpp
+++ drivers/keyboard.cpp
@@ -21,148 +21,48 @@ void KeyboardDriver::activate() {
     Logger::instance().println("[DRIVERS] Keyboard driver initialized");
 }
 
+// Key states for modifiers
+bool shiftPressed = false;
+bool ctrlPressed = false;
+
 void KeyboardDriver::handleInterrupt_() {
     Logger::instance().println("Handling keyboard interrupt");
     uint8_t key = dataPort::read();
+    bool released = key & 0x80; // Check if the key was released
+    key &= 0x7F; // Mask out the release bit to get the scancode
 
-    if (key < 0x80) {
-        switch (key) {
-            case 0x02:
-                handler.onKeyDown('1');
-                break;
-            case 0x03:
-                handler.onKeyDown('2');
-                break;
-            case 0x04:
-                handler.onKeyDown('3');
-                break;
-            case 0x05:
-                handler.onKeyDown('4');
-                break;
-            case 0x06:
-                handler.onKeyDown('5');
-                break;
-            case 0x07:
-                handler.onKeyDown('6');
-                break;
-            case 0x08:
-                handler.onKeyDown('7');
-                break;
-            case 0x09:
-                handler.onKeyDown('8');
-                break;
-            case 0x0A:
-                handler.onKeyDown('9');
-                break;
-            case 0x0B:
-                handler.onKeyDown('0');
-                break;
+    // Handle special keys (Ctrl, Shift, etc.)
+    switch (key) {
+        case 0x1D: // Ctrl key down
+            ctrlPressed = !released;
+            return;
+        case 0x2A: // Left Shift down
+        case 0x36: // Right Shift down
+            shiftPressed = !released;
+            return;
+        default:
+            break; // Not a modifier key, proceed with normal handling
+    }
 
-            case 0x10:
-                handler.onKeyDown('q');
-                break;
-            case 0x11:
-                handler.onKeyDown('w');
-                break;
-            case 0x12:
-                handler.onKeyDown('e');
-                break;
-            case 0x13:
-                handler.onKeyDown('r');
-                break;
-            case 0x14:
-                handler.onKeyDown('t');
-                break;
-            case 0x15:
-                handler.onKeyDown('z');
-                break;
-            case 0x16:
-                handler.onKeyDown('u');
-                break;
-            case 0x17:
-                handler.onKeyDown('i');
-                break;
-            case 0x18:
-                handler.onKeyDown('o');
-                break;
-            case 0x19:
-                handler.onKeyDown('p');
-                break;
+    if (released) {
+        // For keys that we only care about when pressed, not released
+        return;
+    }
 
-            case 0x1E:
-                handler.onKeyDown('a');
-                break;
-            case 0x1F:
-                handler.onKeyDown('s');
-                break;
-            case 0x20:
-                handler.onKeyDown('d');
-                break;
-            case 0x21:
-                handler.onKeyDown('f');
-                break;
-            case 0x22:
-                handler.onKeyDown('g');
-                break;
-            case 0x23:
-                handler.onKeyDown('h');
-                break;
-            case 0x24:
-                handler.onKeyDown('j');
-                break;
-            case 0x25:
-                handler.onKeyDown('k');
-                break;
-            case 0x26:
-                handler.onKeyDown('l');
-                break;
+    KeyMapIndex mapIndex = KeyMapIndex::Normal;
+    if (shiftPressed) {
+        mapIndex = KeyMapIndex::Shift;
+    } else if (ctrlPressed) {
+        mapIndex = KeyMapIndex::Ctrl;
+    }
 
-            case 0x2C:
-                handler.onKeyDown('y');
-                break;
-            case 0x2D:
-                handler.onKeyDown('x');
-                break;
-            case 0x2E:
-                handler.onKeyDown('c');
-                break;
-            case 0x2F:
-                handler.onKeyDown('v');
-                break;
-            case 0x30:
-                handler.onKeyDown('b');
-                break;
-            case 0x31:
-                handler.onKeyDown('n');
-                break;
-            case 0x32:
-                handler.onKeyDown('m');
-                break;
-            case 0x33:
-                handler.onKeyDown(',');
-                break;
-            case 0x34:
-                handler.onKeyDown('.');
-                break;
-            case 0x35:
-                handler.onKeyDown('-');
-                break;
-            case 0x1C:
-                handler.onKeyDown('\n');
-                break;
-            case 0x39:
-                handler.onKeyDown(' ');
-                break;
-
-            default: {
-                // Printer::instance().println("Pressed key %c", (char) key);
-                // Logger::instance().println("Pressed key %c", (char) key);
-                break;
-            }
+    if (key < sizeof(keyboardMap) / sizeof(keyboardMap[0])) {
+        char outputChar = keyboardMap[key][(size_t) mapIndex];
+        if (outputChar != 0) { // Check if there's a mapped character
+            handler.onKeyDown(outputChar);
         }
     }
-    // Printer::instance().println("End of keyboard handler for key %x", (int) key);
-    // Logger::instance().println("End of keyboard handler for key %x", (int) key);
+
 }
 
 void KeyboardDriver::handleInterrupt() {
--- include/drivers/keyboard.h
+++ include/drivers/keyboard.h
@@ -48,3 +48,73 @@ public:
 
     static void handleInterrupt();
 };
+
+// Keyboard map index constants for readability
+enum class KeyMapIndex {
+    Normal = 0,
+    Shift = 1,
+    Ctrl = 2,
+    KeyMapSize = 3 // Placeholder to get the size of the key maps
+};
+
+// Define a keyboard map for the standard QWERTY layout
+constexpr char keyboardMap[0x80][(size_t) KeyMapIndex::KeyMapSize] = {
+        {}, // 0x00
+        {}, // 0x01 ESC
+        {'1', '!', 0}, // 0x02
+        {'2', '@', 0}, // 0x03
+        {'3', '#', 0}, // 0x04
+        {'4', '$', 0}, // 0x05
+        {'5', '%', 0}, // 0x06
+        {'6', '^', 0}, // 0x07
+        {'7', '&', 0}, // 0x08
+        {'8', '*', 0}, // 0x09
+        {'9', '(', 0}, // 0x0A
+        {'0', ')', 0}, // 0x0B
+        {'-', '_', 0}, // 0x0C
+        {'=', '+', 0}, // 0x0D
+        {'\b', '\b', 0x7F}, // 0x0E Backspace
+        {'\t', '\t', 0x09}, // 0x0F Tab
+        {'q', 'Q', 0x11}, // 0x10
+        {'w', 'W', 0x17}, // 0x11
+        {'e', 'E', 0x05}, // 0x12
+        {'r', 'R', 0x12}, // 0x13
+        {'t', 'T', 0x14}, // 0x14
+        {'y', 'Y', 0x19}, // 0x15
+        {'u', 'U', 0x15}, // 0x16
+        {'i', 'I', 0x09}, // 0x17
+        {'o', 'O', 0x0F}, // 0x18
+        {'p', 'P', 0x10}, // 0x19
+        {'[', '{', 0x1B}, // 0x1A
+        {']', '}', 0x1D}, // 0x1B
+        {'\n', '\n', 0x0A}, // 0x1C Enter
+        {}, // 0x1D Ctrl
+        {'a', 'A', 0x01}, // 0x1E
+        {'s', 'S', 0x13}, // 0x1F
+        {'d', 'D', 0x04}, // 0x20
+        {'f', 'F', 0x06}, // 0x21
+        {'g', 'G', 0x07}, // 0x22
+        {'h', 'H', 0x08}, // 0x23
+        {'j', 'J', 0x0A}, // 0x24
+        {'k', 'K', 0x0B}, // 0x25
+        {'l', 'L', 0x0C}, // 0x26
+        {';', ':', 0}, // 0x27
+        {'\'', '\"', 0}, // 0x28
+        {'`', '~', 0}, // 0x29
+        {}, // 0x2A Left Shift
+        {'\\', '|', 0x1C}, // 0x2B
+        {'z', 'Z', 0x1A}, // 0x2C
+        {'x', 'X', 0x18}, // 0x2D
+        {'c', 'C', 0x03}, // 0x2E
+        {'v', 'V', 0x16}, // 0x2F
+        {'b', 'B', 0x02}, // 0x30
+        {'n', 'N', 0x0E}, // 0x31
+        {'m', 'M', 0x0D}, // 0x32
+        {',', '<', 0}, // 0x33
+        {'.', '>', 0}, // 0x34
+        {'/', '?', 0}, // 0
+        {'/', '?', 0}, // 0x35
+        {}, // 0x36 Right Shift
+        {}, // 0x37
+        {' ', ' ', 0}, // 0x38 Space
+};
