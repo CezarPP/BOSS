084b7160977bbb44604033b0496f431388138ba9
cezar <cezarpopoveniuc51@gmail.com>
2024-04-03 17:56:36 +0300

    Add terabytes _TB to literals.h

--- include/util/literals.h
+++ include/util/literals.h
@@ -11,14 +11,18 @@
 
 static_assert(sizeof(size_t) == sizeof(unsigned long long));
 
+inline constexpr size_t operator "" _TB(unsigned long long n) {
+    return n * 1024 * 1024 * 1024 * 1024; // 1 TB = 1024^4 bytes
+}
+
 inline constexpr size_t operator "" _GiB(unsigned long long n) {
-    return n * 1024 * 1024 * 1024;
+    return n * 1024 * 1024 * 1024; // 1 GB = 1024^3 bytes
 }
 
 inline constexpr size_t operator "" _MiB(unsigned long long n) {
-    return n * 1024 * 1024;
+    return n * 1024 * 1024; // 1 MB = 1024^2 bytes
 }
 
 inline constexpr size_t operator "" _KiB(unsigned long long n) {
-    return n * 1024;
+    return n * 1024; // 1 KB = 1024 bytes
 }
