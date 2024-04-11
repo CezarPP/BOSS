ac93fccda36c85e9b6e6adda8b67d0b880569c41
cezar <cezarpopoveniuc51@gmail.com>
2024-04-11 12:02:23 +0300

    Paging!!! Map the kernel space

--- arch/x86_64/paging.cpp
+++ arch/x86_64/paging.cpp
@@ -7,37 +7,23 @@
 
 #include "../../include/arch/x86_64/paging.h"
 #include "../../include/arch/x86_64/logging.h"
+#include "arch/x86_64/exceptions.h"
 
-inline void flushTlb(size_t page) {
-    asm volatile("invlpg [%0]"::"r" (page) : "memory");
+inline __attribute__((always_inline)) void paging::flushTlb(size_t page) {
+    // asm volatile("invlpg [%0]"::"r" (page) : "memory");
+    asm volatile(
+            "invlpg (%0)"              // Invalidate the TLB entry for the given page
+            :                          // No output operands
+            : "r"(page)                // Input operand: page address in a register
+            : "memory"                 // Clobbers: memory
+            );
 }
 
-void paging::clearVirtual(size_t page) {
-    for (size_t i = 0; i < paging::PAGE_SIZE; i++) {
-        reinterpret_cast<uint8_t *>(page)[i] = 0;
-    }
-}
-
-constexpr size_t pml4_entry(size_t virtualAddress) {
-    return (virtualAddress >> 39) & 0x1FF;
-}
-
-constexpr size_t pdpt_entry(size_t virtualAddress) {
-    return (virtualAddress >> 30) & 0x1FF;
-}
-
-constexpr size_t pd_entry(size_t virtualAddress) {
-    return (virtualAddress >> 21) & 0x1FF;
-}
-
-constexpr size_t pt_entry(size_t virtualAddress) {
-    return (virtualAddress >> 12) & 0x1FF;
-}
-
+/*!
+ * Sets the CR3 register to a new value, effectively changing the virtual address space
+ * @param physicalPml4Start The physical address of the lvl 4 Page Table
+ */
 inline __attribute__((always_inline)) void setCR3(size_t physicalPml4Start) {
-    // asm volatile("mov rax, %0; mov cr3, rax" : : "m"(physicalPml4Start) : "memory", "rax");
-    /*asm volatile("mov rax, %0" : : "r"(physicalPml4Start) : "rax"); // First, move to rax
-    asm volatile("mov cr3, rax" : : : "rax"); // Then, move from rax to cr3*/
     static_assert(sizeof(physicalPml4Start) == 8);
     asm volatile(
             "mov %0, %%rax\n\t"          // Move pml4Addr into rax. Use %% for registers in GCC inline asm
@@ -48,54 +34,227 @@ inline __attribute__((always_inline)) void setCR3(size_t physicalPml4Start) {
             );
 }
 
-void paging::init() {
-    Logger::instance().println("Starting to set up paging");
-    clearVirtual(0x400000);
-    // Use the memory at the 2nd Megabyte which is already identity mapped
-    // That is, we already have `paging::IDENTITY_MAPPED_EARLY` identity mapped
-    // This is not very modular, might not work for larger amounts of ram
+/*!
+ * Zeros out a virtual page
+ * @param page The virtual address of the page to be cleared
+ */
+void paging::clearVirtual(size_t page) {
+    for (size_t i = 0; i < paging::PAGE_SIZE; i++) {
+        reinterpret_cast<uint8_t *>(page)[i] = 0;
+    }
+}
+
+namespace paging {
+    /* Use the memory at the 2nd Megabyte which is already identity mapped
+     * That is, we already have `paging::IDENTITY_MAPPED_EARLY` identity mapped
+     * This is not very modular, might not work for larger amounts of ram */
     constexpr auto physical_memory = 2_MiB;
+
+    // We will have the physical page tables after the first 2MB
+    constexpr auto physical_pml4t_start = physical_memory;
+    // Tables are identity mapped
+    constexpr auto virtual_pml4t_start = physical_pml4t_start;
 
     // The number of entries of the PML4 Table (Page Map Level 4)
     constexpr auto pml4_entries = entries(KERNEL_VIRTUAL_SIZE, pml4e_allocations);
     constexpr auto pdpt_entries = entries(KERNEL_VIRTUAL_SIZE, pdpte_allocations);
     constexpr auto pd_entries = entries(KERNEL_VIRTUAL_SIZE, pde_allocations);
 
-    Logger::instance().println("The number of pd_entries is %d", pd_entries);
-
-    // We will have the physical page tables after the first 2MB
-    constexpr auto physical_pml4t_start = physical_memory;
-    // The PML4 table will only be one page
-    // So, the first Page-Directory-Pointer Table (PDPT) will start after one page
+    /* The PML4 table will only be one page
+     So, the first Page-Directory-Pointer Table (PDPT) will start after one page*/
     constexpr auto physical_pdpt_start = physical_pml4t_start + paging::PAGE_SIZE;
     // There will be as many Page Directory (PD) Tables as the number of entries in the PML4 tables
     constexpr auto physical_pd_start = physical_pdpt_start + pml4_entries * paging::PAGE_SIZE;
     // There will be as many Page Tables (PT) as the number of entries in the Page Directory Tables (PD)
     constexpr auto physical_pt_start = physical_pd_start + pdpt_entries * paging::PAGE_SIZE;
 
-    // We will assume we can map all of these tables in the next 6MB of memory, starting from physical_memory
-    // This static_assert ensures this holds
+    constexpr auto virtual_pdpt_start = physical_pdpt_start;
+    constexpr auto virtual_pd_start = physical_pd_start;
+    constexpr auto virtual_pt_start = physical_pt_start;
+
+
+    pml4t_t find_pml4t() {
+        return reinterpret_cast<paging::pml4t_t>(paging::virtual_pml4t_start);
+    }
+}
+
+/*
+ * Entries in the page tables contain the physical address of the next page table.
+ * That is, if they were to contain a virtual address, the address finding process would start all over.
+ */
+
+paging::pdpt_t find_pdpt(paging::pml4t_t pml4t, size_t pml4e) {
+    auto physical_pdpt = reinterpret_cast<uintptr_t>(pml4t[pml4e]) & ~0xFFF;
+    auto physical_offset = physical_pdpt - paging::physical_pdpt_start;
+    auto virtual_pdpt = paging::virtual_pdpt_start + physical_offset;
+    return reinterpret_cast<paging::pdpt_t>(virtual_pdpt);
+}
+
+paging::pd_t find_pd(paging::pdpt_t pdpt, size_t pdpte) {
+    auto physical_pd = reinterpret_cast<uintptr_t>(pdpt[pdpte]) & ~0xFFF;
+    auto physical_offset = physical_pd - paging::physical_pd_start;
+    auto virtual_pd = paging::virtual_pd_start + physical_offset;
+    return reinterpret_cast<paging::pd_t>(virtual_pd);
+}
+
+paging::pt_t find_pt(paging::pd_t pd, size_t pde) {
+    auto physical_pt = reinterpret_cast<uintptr_t>(pd[pde]) & ~0xFFF;
+    auto physical_offset = physical_pt - paging::physical_pt_start;
+    auto virtual_pt = paging::virtual_pt_start + physical_offset;
+    return reinterpret_cast<paging::pt_t>(virtual_pt);
+}
+
+//TODO Improve to support a status
+size_t paging::physicalAddress(VirtualAddress virt) {
+    if (!pagePresent(virt)) {
+        return 0;
+    }
+
+    auto pml4t = find_pml4t();
+    auto pdpt = find_pdpt(pml4t, virt.p4Index);
+    auto pd = find_pd(pdpt, virt.p3Index);
+    auto pt = find_pt(pd, virt.p2Index);
+    Logger::instance().println("PML4T %X, pdpt %X, pd %X, pt %X", pml4t, pdpt, pd, pt);
+
+    return virt.offset + (reinterpret_cast<uintptr_t>(pt[virt.p1Index]) & ~0xFFF);
+}
+
+bool paging::pagePresent(VirtualAddress virt) {
+    auto pml4t = find_pml4t();
+    if (!(reinterpret_cast<uintptr_t>(pml4t[virt.p4Index]) & PRESENT)) {
+        return false;
+    }
+
+    auto pdpt = find_pdpt(pml4t, virt.p4Index);
+    if (!(reinterpret_cast<uintptr_t>(pdpt[virt.p3Index]) & PRESENT)) {
+        return false;
+    }
+
+    auto pd = find_pd(pdpt, virt.p3Index);
+    if (!(reinterpret_cast<uintptr_t>(pd[virt.p2Index]) & PRESENT)) {
+        return false;
+    }
+
+    auto pt = find_pt(pd, virt.p2Index);
+    return reinterpret_cast<uintptr_t>(pt[virt.p1Index]) & PRESENT;
+}
+
+void paging::map(VirtualAddress virt, size_t physical, uint8_t flags) {
+    // The address must be page-aligned
+    kAssert(virt.isPageAligned(), "Page is not page-aligned");
+
+    auto pml4t = find_pml4t();
+    Logger::instance().println("[PAGING] Mapping pml4e %X, pdpte %X, pde %X, pte %X",
+                               virt.p4Index, virt.p3Index, virt.p2Index, virt.p1Index);
+    kAssert(reinterpret_cast<uintptr_t>(pml4t[virt.p4Index]) & PRESENT, "[PAGING] A PML4T entry is not PRESENT");
+
+    auto pdpt = find_pdpt(pml4t, virt.p4Index);
+    kAssert(reinterpret_cast<uintptr_t>(pdpt[virt.p3Index]) & PRESENT, "[PAGING] A PDPT entry is not PRESENT");
+
+    auto pd = find_pd(pdpt, virt.p3Index);
+    kAssert(reinterpret_cast<uintptr_t>(pd[virt.p2Index]) & PRESENT, "[PAGING] A PD entry is not PRESENT");
+
+    auto pt = find_pt(pd, virt.p2Index);
+
+    // Check if the page is already present
+    if (reinterpret_cast<uintptr_t>(pt[virt.p1Index]) & PRESENT) {
+        // Check that the page is set to the correct value
+        kAssert(reinterpret_cast<uintptr_t>(pt[virt.p1Index]) == (physical | flags),
+                "[PAGING] Page has incorrect flags");
+    }
+
+    // Map to the physical address
+    pt[virt.p1Index] = reinterpret_cast<page_entry>(physical | flags);
+
+    // Flush the TLB
+    flushTlb(virt.address);
+}
+
+void paging::mapPages(VirtualAddress virt, size_t physical, size_t pages, uint8_t flags) {
+    // The address must be page-aligned
+    kAssert(virt.isPageAligned(), "[PAGING] Page is not page-aligned");
+
+    // Map each page
+    for (size_t page = 0; page < pages; page++) {
+        auto virt_addr = virt.address + page * PAGE_SIZE;
+        auto phys_addr = physical + page * PAGE_SIZE;
+
+        map(virt_addr, phys_addr, flags);
+    }
+}
+
+void paging::unmap(VirtualAddress virt) {
+    // The address must be page-aligned
+    kAssert(virt.isPageAligned(), "[PAGING] Page is not page aligned");
+
+    auto pml4t = find_pml4t();
+
+    // If not present, return
+    if (!(reinterpret_cast<uintptr_t>(pml4t[virt.p4Index]) & PRESENT)) {
+        return;
+    }
+
+    auto pdpt = find_pdpt(pml4t, virt.p4Index);
+
+    // If not present, return
+    if (!(reinterpret_cast<uintptr_t>(pdpt[virt.p3Index]) & PRESENT)) {
+        return;
+    }
+
+    auto pd = find_pd(pdpt, virt.p3Index);
+
+    // If not present, return
+    if (!(reinterpret_cast<uintptr_t>(pd[virt.p2Index]) & PRESENT)) {
+        return;
+    }
+
+    auto pt = find_pt(pd, virt.p2Index);
+
+    // Unmap the virtual address
+    pt[virt.p1Index] = 0x0;
+
+    // Flush TLB
+    flushTlb(virt.address);
+}
+
+void paging::unmapPages(VirtualAddress virt, size_t pages) {
+    /// The address must be page-aligned
+    kAssert(virt.isPageAligned(), "[PAGING] Page is not page aligned");
+
+    /// Unmap each page
+    for (size_t page = 0; page < pages; page++) {
+        auto virt_addr = virt.address + page * PAGE_SIZE;
+        unmap(virt_addr);
+    }
+}
+
+void paging::init() {
+    Logger::instance().println("[PAGING] Setting up paging...");
+    // clearVirtual(0x400000);
+
+    /// We will assume we can map all of these tables in the next 6MB of memory, starting from physical_memory
+    /// This static_assert ensures this holds
     static_assert(physical_pt_start + pd_entries * PAGE_SIZE < IDENTITY_MAPPED_EARLY);
 
     auto flags = PRESENT | WRITE | USER;
 
     /// 1. Prepare PML4T - Page Map Level 4
-    Logger::instance().println("Starting to prepare PML4T");
+    Logger::instance().println("[PAGING] Preparing PML4T, mapping %X entries", pml4_entries);
 
     clearVirtual(physical_pml4t_start); // It is identity mapped
     auto virtual_pml4t = reinterpret_cast<pml4t_t>(physical_pml4t_start);
-    for (size_t i = 0; i < pml4_entries; ++i) {
+    for (size_t i = 0; i < pml4_entries; i++) {
         virtual_pml4t[i] = reinterpret_cast<pdpt_t>((physical_pdpt_start + i * PAGE_SIZE) | flags);
     }
 
     /// 2. Prepare each PDPT - Page Directory Pointer Table
-    Logger::instance().println("Starting to prepare PDPT");
+    Logger::instance().println("[PAGING] Preparing PDPT, mapping %X entries", pdpt_entries);
 
-    for (size_t i = 0; i < pml4_entries; ++i) {
+    for (size_t i = 0; i < pml4_entries; i++) {
         auto virtual_pdpt = reinterpret_cast<pdpt_t>(physical_pdpt_start + i * PAGE_SIZE);
         clearVirtual(physical_pdpt_start + i * PAGE_SIZE);
 
-        for (size_t j = 0; j + i * 512 < pdpt_entries; ++j) {
+        for (size_t j = 0; j + i * 512 < pdpt_entries; j++) {
             auto r = j + i * 512;
 
             virtual_pdpt[j] = reinterpret_cast<pd_t>((physical_pd_start + r * PAGE_SIZE) | flags);
@@ -104,13 +263,13 @@ void paging::init() {
 
 
     /// 3. Prepare each PD - Page Directory
-    Logger::instance().println("Starting to prepare PD");
+    Logger::instance().println("[PAGING] Preparing PD, mapping %X entries", pd_entries);
 
-    for (size_t i = 0; i < pdpt_entries; ++i) {
+    for (size_t i = 0; i < pdpt_entries; i++) {
         auto virtual_pd = reinterpret_cast<pd_t>(physical_pd_start + i * PAGE_SIZE);
         clearVirtual(physical_pd_start + i * PAGE_SIZE);
 
-        for (size_t j = 0; j + i * 512 < pd_entries; ++j) {
+        for (size_t j = 0; j + i * 512 < pd_entries; j++) {
             auto r = j + i * 512;
 
             virtual_pd[j] = reinterpret_cast<pt_t>((physical_pt_start + r * PAGE_SIZE) | flags);
@@ -118,30 +277,53 @@ void paging::init() {
     }
 
     /// 4. Prepare each PT - Page Table
-    Logger::instance().println("Starting to prepare PT, number of PD entries is %d", pd_entries);
-    for (size_t i = 0; i < pd_entries; ++i) {
+    Logger::instance().println("[PAGING] Preparing PT, mapping %X entries...", pd_entries);
+    for (size_t i = 0; i < pd_entries; i++) {
         auto pageToClear = physical_pt_start + i * PAGE_SIZE;
         clearVirtual(pageToClear);
     }
 
-    /// 5. Identity map the first 8MB
-    Logger::instance().println("Starting to do identity mapping");
+    /// 5. Identity map the first IDENTITY_MAPPED_EARLY MB (8MB)
+    Logger::instance().println("[PAGING] Doing identity mapping on the first %X bytes...", IDENTITY_MAPPED_EARLY);
 
     auto pageTableEntry = reinterpret_cast<uint64_t *>(physical_pt_start);
-    auto phys = PRESENT | WRITE; // Flags for each Page Table Entry
+    uint64_t phys = PRESENT | WRITE; // Flags for each Page Table Entry
     // We need to identity map 8MB and the page entries map 4KB each
     // We are assuming these Page Tables are in continuous physical (and virtual) memory
-    for (size_t i = 0; i < 8_MiB / 4_KiB; ++i, ++pageTableEntry) {
+    for (size_t i = 0; i < IDENTITY_MAPPED_EARLY / PAGE_SIZE; i++, pageTableEntry++) {
         // Set Page Table Entry
         *pageTableEntry = phys;
         // Map next page
         phys += paging::PAGE_SIZE;
     }
 
-    Logger::instance().println("Setting new CR3 to %X\n", physical_pml4t_start);
+    Logger::instance().println("[PAGING] Setting new CR3 to %X...", physical_pml4t_start);
 
     static_assert(sizeof(physical_pml4t_start) == 8);
+
     setCR3(physical_pml4t_start);
 
-    Logger::instance().println("New cr3 has been set!");
+    Logger::instance().println("[PAGING] New cr3 has been set!\n");
+
+    /// 6. Perform some tests to check whether the mapping works
+
+    Logger::instance().println("[PAGING] Testing...");
+
+    kAssert(pagePresent(virtual_pml4t_start), "[PAGING] PML4T is not present");
+
+    Logger::instance().println("Virtual address for PML4T is %X", virtual_pml4t_start);
+    Logger::instance().println("Physical address for virtual PML4T is %X", physicalAddress(virtual_pml4t_start));
+    Logger::instance().println("Physical address for virtual 0x0 is %X", physicalAddress(0x0));
+
+
+    kAssert(physicalAddress(0x0) == 0x0, "[PAGING] Invalid identity mapping of the first MiB");
+
+    kAssert(physicalAddress(virtual_pml4t_start) == physical_pml4t_start, "[PAGING] PML4T is not correctly mapped");
+
+    kAssert(physicalAddress(0x6969) == 0x6969, "[PAGING] Invalid identity mapping of the first MiB");
+
+    kAssert(physicalAddress(4_MiB) == 4_MiB, "[PAGING] Invalid identity mapping of the first 8MiB");
+
+
+    Logger::instance().println("[PAGING] Tests finished");
 }
--- include/arch/x86_64/paging.h
+++ include/arch/x86_64/paging.h
@@ -6,36 +6,55 @@
  */
 
 /*
- * Early paging that is set up in main.asm identity maps the first 4MB (2 huge pages).
+ * Early paging that is set up in main.asm identity maps the first 8MB (4 huge pages).
  * The stack initially consists of 2 page-frames = 2 * 4KB = 8KB
  * The initial 4-level paging tables are 4 * 4KB = 16KB
+ */
+
+/*
+ * Tables will be accessed through their virtual addresses.
  */
 
 #include "util/literals.h"
 #include "std/algorithm.h"
 #include "phys_mm.h"
+#include "std/utility.h"
+#include "phys_mm.h"
 
 namespace paging {
-    // Hopefully:
-    // User Space: 0x0000000000000000 to 0x00007fffffffffff
-    // Kernel Space: 0xffff800000000000 to 0xffffffffffffffff
+    /* Future work:
+     * User Space: 0x0000000000000000 to 0x00007fffffffffff
+     * Theoretical Kernel Space: 0xffff800000000000 to 0xffffffffffffffff
+     * Practical Kernel Space: 0xffff800000000000 to 0xffff800077359400 for 2GB virtual size */
+
+    /* Current config
+     * Kernel Space: 0x0000000000000000 to 2GB
+     * User Space: not implemented
+     */
+
 
     constexpr const size_t IDENTITY_MAPPED_EARLY = 8_MiB;
     constexpr const size_t PAGE_SIZE = 4_KiB;
     constexpr const size_t KERNEL_VIRTUAL_SIZE = 2_GiB;
+    constexpr const size_t MAX_PHYSICAL_SIZE = 32_GiB;
 
-    struct TableInterface {
-        PageEntry pageEntries[PAGE_SIZE / sizeof(PageEntry)];
-        static_assert(PAGE_SIZE / sizeof(PageEntry) == 512);
-    };
+    // Start of the virtual addresses for the structures of the allocator
+    constexpr const size_t PHYSICAL_ALLOCATOR_VIRTUAL_START = 8_MiB;
+    // Linux uses 1% of memory to keep the page_frame structs, we will make sure not to use more than that
+    // Don't need this constant anymore
+    // constexpr const size_t PHYSICAL_ALLOCATOR_VIRTUAL_SIZE = MAX_PHYSICAL_SIZE / 100;
 
-    typedef uint64_t* page_entry;
-    typedef page_entry* pt_t;
-    typedef pt_t* pd_t;
-    typedef pd_t* pdpt_t;
-    typedef pdpt_t* pml4t_t;
+    /*constexpr const size_t KERNEL_VIRTUAL_START = PHYSICAL_ALLOCATOR_VIRTUAL_START + PHYSICAL_ALLOCATOR_VIRTUAL_SIZE;
+    // The start and end virtual addresses available to the Virtual Allocator
+    // Only for kernel space, user space should be able to address more since it will have more page tables
+    constexpr const size_t VIRTUAL_ALLOCATOR_START = KERNEL_VIRTUAL_START + PHYSICAL_ALLOCATOR_VIRTUAL_SIZE;
+    constexpr const size_t VIRTUAL_ALLOCATOR_END = KERNEL_VIRTUAL_START + KERNEL_VIRTUAL_SIZE;*/
 
-    static_assert(AssertSize<TableInterface, PAGE_SIZE>());
+    typedef uint64_t *page_entry;
+    typedef page_entry *pt_t;
+    typedef pt_t *pd_t;
+    typedef pd_t *pdpt_t;
+    typedef pdpt_t *pml4t_t;
 
     // On each page there are
     constexpr const size_t pml4e_allocations = 512_GiB; /// The physical memory that a PML4T Entry can map
@@ -43,16 +62,53 @@ namespace paging {
     constexpr const size_t pde_allocations = 2_MiB;   /// The physical memory that a PD Entry can map
     constexpr const size_t pte_allocations = 4_KiB;   /// The physical memory that a PD Entry can map
 
-    constexpr const uint8_t PRESENT        = 0x1;  /// Paging flag for present page
-    constexpr const uint8_t WRITE          = 0x2;  /// Paging flag for writable page
-    constexpr const uint8_t USER           = 0x4;  /// Paging flag for user page
-    constexpr const uint8_t WRITE_THROUGH  = 0x8;  /// Paging flag for write-through page
+    constexpr const uint8_t PRESENT = 0x1;  /// Paging flag for present page
+    constexpr const uint8_t WRITE = 0x2;  /// Paging flag for writable page
+    constexpr const uint8_t USER = 0x4;  /// Paging flag for user page
+    constexpr const uint8_t WRITE_THROUGH = 0x8;  /// Paging flag for write-through page
     constexpr const uint8_t CACHE_DISABLED = 0x10; /// Paging flag for cache disabled page
-    constexpr const uint8_t ACCESSED       = 0x20; /// Paging flag for assessed page
+    constexpr const uint8_t ACCESSED = 0x20; /// Paging flag for accessed page
 
-    // Computes the number of entries necessary to map a certain size of memory
+    size_t physicalAddress(VirtualAddress virt);
+
+    bool pagePresent(VirtualAddress virt);
+
+    pml4t_t find_pml4t();
+
+    void map(VirtualAddress virt, size_t physical, uint8_t flags = PRESENT | WRITE);
+
+    void mapPages(VirtualAddress virt, size_t physical, size_t pages, uint8_t flags = PRESENT | WRITE);
+
+    void unmap(VirtualAddress virt);
+
+    void unmapPages(VirtualAddress virt, size_t pages);
+
+    /*!
+     * Computes the number of entries necessary to map a certain size of memory
+     *
+     * The memory could be anything, while the entry could be the size of a page for example
+     * @param mem The size of the memory to be mapped
+     * @param entry_size The size of the entry
+     * @return The number of entries necessary
+     */
     constexpr size_t entries(size_t mem, size_t entry_size) {
         return std::max((size_t) 1, mem / entry_size + ((mem % entry_size == 0) ? 0 : 1));
+    }
+
+    constexpr bool pageAligned(size_t addr) {
+        return !(addr & (paging::PAGE_SIZE - 1));
+    }
+
+    constexpr size_t pageAlign(size_t addr) {
+        return (addr / paging::PAGE_SIZE) * paging::PAGE_SIZE;
+    }
+
+    constexpr std::pair<uint64_t, uint64_t> physicalExcludingEarly(const std::pair<uint64_t, uint64_t> &memory) {
+        if (memory.first < IDENTITY_MAPPED_EARLY) {
+            uint64_t dif = IDENTITY_MAPPED_EARLY - memory.first;
+            return {IDENTITY_MAPPED_EARLY, memory.second - dif};
+        }
+        return memory;
     }
 
     // Initialize paging
@@ -61,5 +117,5 @@ namespace paging {
     // Clear all the bytes given the virtual address of a page
     void clearVirtual(size_t page);
 
-    void flushTlb(size_t page);
+    __attribute__((always_inline)) void flushTlb(size_t page);
 }
