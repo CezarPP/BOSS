56a62504f8542ace39025b51c73477959552ab0d
cezar <cezarpopoveniuc51@gmail.com>
2024-04-17 12:11:49 +0300

    Placement new

---
+++ include/std/new.h
@@ -0,0 +1,19 @@
+/*
+ * new.h
+ *
+ *  Created on: 4/13/24.
+ *      Author: Cezar PP
+ */
+
+#pragma once
+
+#include "cstddef.h"
+
+// Placement new
+inline void *operator new(size_t, void *place) noexcept {
+    return place;
+}
+
+inline void *operator new[](size_t, void *place) noexcept {
+    return place;
+}
