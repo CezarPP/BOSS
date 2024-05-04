f33ab12377e07289023243ffd1571ea32a561b74
cezar <cezarpopoveniuc51@gmail.com>
2024-05-04 13:00:08 +0300

    main() to reflect new developments

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -4,6 +4,9 @@
 #include "util/console_printer.h"
 #include "multiboot/multiboot.h"
 #include "allocators/virtual_allocator.h"
+#include "std/new.h"
+#include "std/initializer_list.h"
+#include "std/vector.h"
 
 void f() {
     // Logger::instance().println("[MAIN] IN F, x=%X", reinterpret_cast<uint64_t>(&x));
@@ -13,13 +16,12 @@ void f() {
 extern "C" void kernel_main(uint64_t multibootAndMagic) {
     // [PRINTER] Initialize printer
     Printer::instance().print_clear();
-    Printer::instance().printSetColor(PRINTER_COLORS::PRINT_COLOR_YELLOW, PRINTER_COLORS::PRINT_COLOR_BLACK);
     Printer::instance().println("[KERNEL MAIN] Welcome to BOSS!\n");
 
 
     // [LOGGER]
     if (Logger::instance().init()) {
-        Printer::instance().println("Logger could not be initialized\nExiting...");
+        Logger::instance().println("Logger could not be initialized\nExiting...");
         return;
     }
 
@@ -42,6 +44,7 @@ extern "C" void kernel_main(uint64_t multibootAndMagic) {
 
     // [MULTIBOOT]
     std::pair<uint64_t, uint64_t> memory = parseMultiboot(multibootAndMagic);
+    memory = paging::physicalExcludingEarly(memory);
 
     // [PAGING]
     paging::init();
@@ -49,10 +52,10 @@ extern "C" void kernel_main(uint64_t multibootAndMagic) {
     // [PHYSICAL ALLOCATOR] [VIRTUAL ALLOCATOR]
     // The virtual allocator also initializes the physical allocator
     Logger::instance().println("[MAIN] Starting to initialize the Physical and Virtual allocators");
-    memory = paging::physicalExcludingEarly(memory);
-    VirtualAllocator virtualAllocator(memory.first, memory.second);
+    VirtualAllocator::init(memory.first, memory.second);
     Logger::instance().println("[MAIN] Physical and Virtual allocators have been initialized");
 
+    // From now on we can call malloc() and new
 
     while (true) {
         haltCpu();
