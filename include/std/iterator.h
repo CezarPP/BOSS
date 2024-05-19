05294fb3fa14c4c27e20e205ef4079cd32a6c607
cezar <cezarpopoveniuc51@gmail.com>
2024-05-19 17:40:38 +0300

    std::reverse_iterator<>

--- include/std/iterator.h
+++ include/std/iterator.h
@@ -16,4 +16,42 @@ namespace std {
         // Can be improved upon
         return end - it;
     }
+
+    template<typename Iterator>
+    struct reverse_iterator {
+        using iterator_type = Iterator;
+        using value_type = typename std::iterator_traits<Iterator>::value_type;
+        using difference_type = typename std::iterator_traits<Iterator>::difference_type;
+        using pointer = typename std::iterator_traits<Iterator>::pointer;
+        using reference = typename std::iterator_traits<Iterator>::reference;
+
+        reverse_iterator(Iterator it)
+                : it(it) {
+        }
+
+        reference operator*() {
+            return *it;
+        }
+
+        reverse_iterator &operator++() {
+            --it;
+            return *this;
+        }
+
+        reverse_iterator &operator--() {
+            ++it;
+            return *this;
+        }
+
+        bool operator==(const reverse_iterator &rhs) {
+            return it == rhs.it;
+        }
+
+        bool operator!=(const reverse_iterator &rhs) {
+            return it != rhs.it;
+        }
+
+    private:
+        iterator_type it;
+    };
 }
