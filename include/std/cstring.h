f2cbe13f4b5658a428cb0534227ac1d496539a48
cezar <cezarpopoveniuc51@gmail.com>
2024-05-19 17:40:10 +0300

    strlen()

--- include/std/cstring.h
+++ include/std/cstring.h
@@ -9,3 +9,11 @@ void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void *s, int c, size_t n);
 
 int memcmp(const void *s1, const void *s2, size_t n);
+
+inline uint64_t strlen(const char *a) {
+    uint64_t length = 0;
+    while (*a++) {
+        ++length;
+    }
+    return length;
+}
