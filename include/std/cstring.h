572709180ea3653d63df44751744bc47f412de25
cezar <cezarpopoveniuc51@gmail.com>
2024-05-20 18:21:50 +0300

    Add strncmp()

--- include/std/cstring.h
+++ include/std/cstring.h
@@ -1,7 +1,21 @@
 #pragma once
+
 #include <stdint.h>
 
 typedef uint64_t size_t;
+
+inline int strncmp(const char *str1, const char *str2, size_t n) {
+    while (n && *str1 && (*str1 == *str2)) {
+        ++str1;
+        ++str2;
+        --n;
+    }
+    if (n == 0) {
+        return 0;
+    } else {
+        return (*(unsigned char *) str1 - *(unsigned char *) str2);
+    }
+}
 
 inline void *memcpy(void *dest, const void *src, size_t n) {
     auto d = static_cast<char *>(dest);
