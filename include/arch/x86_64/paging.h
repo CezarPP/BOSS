476cacbc1448e75792b64f93b77676b89319b4e3
cezar <cezarpopoveniuc51@gmail.com>
2024-06-08 13:49:01 +0300

    Split into paging.h & paging_constants.h

--- include/arch/x86_64/paging.h
+++ include/arch/x86_64/paging.h
@@ -15,11 +15,11 @@
  * Tables will be accessed through their virtual addresses.
  */
 
-#include "util/literals.h"
 #include "std/algorithm.h"
 #include "phys_mm.h"
 #include "logging.h"
 #include "exceptions.h"
+#include "paging_constants.h"
 
 namespace paging {
     /* Future work:
@@ -32,42 +32,11 @@ namespace paging {
      * User Space: not implemented
      */
 
-
-    constexpr const size_t IDENTITY_MAPPED_EARLY = 8_MiB;
-    constexpr const size_t PAGE_SIZE = 4_KiB;
-    constexpr const size_t KERNEL_VIRTUAL_SIZE = 2_GiB;
-    constexpr const size_t MAX_PHYSICAL_SIZE = 32_GiB;
-
-    // Start of the virtual addresses for the structures of the allocator
-    constexpr const size_t PHYSICAL_ALLOCATOR_VIRTUAL_START = 8_MiB;
-    // Linux uses 1% of memory to keep the page_frame structs, we will make sure not to use more than that
-    // Don't need this constant anymore
-    // constexpr const size_t PHYSICAL_ALLOCATOR_VIRTUAL_SIZE = MAX_PHYSICAL_SIZE / 100;
-
-    /*constexpr const size_t KERNEL_VIRTUAL_START = PHYSICAL_ALLOCATOR_VIRTUAL_START + PHYSICAL_ALLOCATOR_VIRTUAL_SIZE;
-    // The start and end virtual addresses available to the Virtual Allocator
-    // Only for kernel space, user space should be able to address more since it will have more page tables
-    constexpr const size_t VIRTUAL_ALLOCATOR_START = KERNEL_VIRTUAL_START + PHYSICAL_ALLOCATOR_VIRTUAL_SIZE;
-    constexpr const size_t VIRTUAL_ALLOCATOR_END = KERNEL_VIRTUAL_START + KERNEL_VIRTUAL_SIZE;*/
-
     typedef uint64_t *page_entry;
     typedef page_entry *pt_t;
     typedef pt_t *pd_t;
     typedef pd_t *pdpt_t;
     typedef pdpt_t *pml4t_t;
-
-    // On each page there are
-    constexpr const size_t pml4e_allocations = 512_GiB; /// The physical memory that a PML4T Entry can map
-    constexpr const size_t pdpte_allocations = 1_GiB;   /// The physical memory that a PDPT Entry can map
-    constexpr const size_t pde_allocations = 2_MiB;   /// The physical memory that a PD Entry can map
-    constexpr const size_t pte_allocations = 4_KiB;   /// The physical memory that a PD Entry can map
-
-    constexpr const uint8_t PRESENT = 0x1;  /// Paging flag for present page
-    constexpr const uint8_t WRITE = 0x2;  /// Paging flag for writable page
-    constexpr const uint8_t USER = 0x4;  /// Paging flag for user page
-    constexpr const uint8_t WRITE_THROUGH = 0x8;  /// Paging flag for write-through page
-    constexpr const uint8_t CACHE_DISABLED = 0x10; /// Paging flag for cache disabled page
-    constexpr const uint8_t ACCESSED = 0x20; /// Paging flag for accessed page
 
     size_t physicalAddress(VirtualAddress virt);
 
@@ -103,7 +72,7 @@ namespace paging {
         return (addr / paging::PAGE_SIZE) * paging::PAGE_SIZE;
     }
 
-    constexpr std::pair<uint64_t, uint64_t> physicalExcludingEarly(const std::pair<uint64_t, uint64_t> &memory) {
+    inline constexpr std::pair<uint64_t, uint64_t> physicalExcludingEarly(const std::pair<uint64_t, uint64_t> &memory) {
         if (memory.first < IDENTITY_MAPPED_EARLY) {
             uint64_t dif = IDENTITY_MAPPED_EARLY - memory.first;
             return {IDENTITY_MAPPED_EARLY, memory.second - dif};
